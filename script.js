e0103e68e1ada777e86cab2f6c50b3e2ac210155
dagiStark <dagigelaneh26@gmail.com>
2024-03-24 18:51:44 +0300

    added functions to open and close the modal, 'Esc'  key  will close the modal.

--- script.js
+++ script.js
@@ -88,4 +88,34 @@ function playRound(event){
 
 
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const openModalButton = document.querySelector('.btn-open');
+const closeModalButton = document.querySelector('.btn-close');
+
+
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+
+openModalButton.addEventListener('click', openModal);
+
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+
+closeModalButton.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+
+document.addEventListener('keydown', function(event){
+    if(event.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModal();
+    }
+});
 
